43a408552890deebfea8f89c6a36e697989a805e
Alan King <alanking@renci.org>
2022-04-06 14:58:56 -0400

    don't overload sys.stdout

--- irods_python_ci_utilities/irods_python_ci_utilities.py
+++ irods_python_ci_utilities/irods_python_ci_utilities.py
@@ -42,21 +42,21 @@ def subprocess_get_output(*args, **kwargs):
     p = subprocess.Popen(*args, **kwargs)
     out_b, err_b = p.communicate(data)
 
-    sys.stdout = codecs.getwriter('utf-8')(sys.stdout.detach())
+    stdout = codecs.getwriter('utf-8')(sys.stdout.detach())
 
     if out_b:
         out = out_b.decode('utf-8') if isinstance(out_b, bytes) else out_b
-        sys.stdout.write(out)
+        stdout.write(out)
     else:
         out = ''
 
     if err_b:
         err = err_b.decode('utf-8') if isinstance(err_b, bytes) else err_b
-        sys.stdout.write(err)
+        stdout.write(err)
     else:
         err = ''
 
-    sys.stdout.flush()
+    stdout.flush()
 
     if check_rc:
         if p.returncode != 0:
